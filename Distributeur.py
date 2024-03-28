5ab50916737e5ca0893a4a5ea301fb357b3f755f
LIKELENGE MOISE <fatakilikelenge@gmail.com>
2024-03-28 17:00:44 +0100

    Modification de la mise à jour

--- Distributeur.py
+++ Distributeur.py
@@ -1,34 +1,26 @@
-rep = input("Voulez-vous acheter une autre boisson o/n ?")
-rep = "o"
-while rep == "o":
-    stock_soda = 3
-    stock_orangeade = 5
-    stock_eau = 0
+LIMITE_MAX = 3
 
-    print("Veuillez sélectionner une boisson :")
-    print("1. Soda")
-    print("2. Orangeade")
-    print("3. Eau")
+stocks = [3, 0, 2]
+noms = ["Eau", "Soda", "Orangeade"]
+
+totalStock = sum(stocks)
+
+choix = 0
+
+while totalStock > 0 and choix != 4:
+    print("Choisissez une boisson.")
+    for i in range(LIMITE_MAX):
+        print(noms[i] + " : " + str(i+1))
+    print("FINIR : 4")
     choix = int(input())
 
-    if choix == 1:
-        if stock_soda > 0:
-            print("A votre santé, voici votre Soda!")
-            stock_soda -= 1
+    if choix != 4:
+        if stocks[choix-1] > 0:
+            print("Voici votre " + noms[choix-1] + " , santé!")
+            stocks[choix-1] -= 1
         else:
-            print("Sold out! Faites un autre choix...")
-    elif choix == 2:
-        if stock_orangeade > 0:
-            print("A votre santé, voici votre orangeade!")
-            stock_orangeade -= 1
-        else:
-            print("Sold out! Faites un autre choix...")
-    elif choix == 3:
-        if stock_eau > 0:
-            print("A votre santé, voici votre eau!")
-            stock_eau -= 1
-        else:
-            print("Sold out! Faites un autre choix...")
-    else:
-        print("Erreur, boisson non-disponible...")
-    rep = input("Voulez-vous acheter une autre boisson o/n ?")
+            print("Plus de "+ noms[choix-1])
+
+    totalStock = sum(stocks)
+
+print("Merci d'utiliser distributeur 3000!")
